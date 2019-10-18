34046c81d5dbc32a46e83d335eff1b4b98ee58e6
William Baquero <williambaqueror@gmail.com>
2019-10-18 16:30:00 -0500

    boton lista

--- Cliente/app/src/main/java/com/example/clienteapp/MapaActivity.java
+++ Cliente/app/src/main/java/com/example/clienteapp/MapaActivity.java
@@ -2,7 +2,10 @@ package com.example.clienteapp;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,6 +17,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapaActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    Button btnLista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,14 @@ public class MapaActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        btnLista = findViewById(R.id.btnLista);
+        btnLista.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MapaActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
