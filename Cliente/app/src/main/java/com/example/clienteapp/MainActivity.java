89e75fa24c30cd05ff030d4aa8e3f67f899eefbc
William Baquero <williambaqueror@gmail.com>
2019-10-17 23:08:04 -0500

    Ingreso Usuario

--- Cliente/app/src/main/java/com/example/clienteapp/MainActivity.java
+++ Cliente/app/src/main/java/com/example/clienteapp/MainActivity.java
@@ -1,21 +1,49 @@
 package com.example.clienteapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private Button ingresar;
+    private EditText password;
+    private EditText email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.correo);
+        password = findViewById(R.id.psw);
+        ingresar = findViewById(R.id.btnIngresar);
+        ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isEmailValid(email.getText().toString())){
+                    signInUser(email.getText().toString(),password.getText().toString());
+                }else{
+                    Toast.makeText(MainActivity.this, "Email no valido.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 
     @Override
@@ -24,5 +52,60 @@ public class MainActivity extends AppCompatActivity {
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
         updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser currentUser){
+        if(currentUser!=null){
+            Intent intent = new Intent(getBaseContext(), HomeActivity.class);
+            intent.putExtra("user", currentUser.getEmail());
+            startActivity(intent);
+        } else {
+            email.setText("");
+            password.setText("");
+        }
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+        String email = this.email.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            this.email.setError("Required.");
+            valid = false;
+        } else {
+            this.email.setError(null);
+        }
+        String password = this.password.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            this.password.setError("Required.");
+            valid = false;
+        } else {
+            this.password.setError(null);
+        }
+        return valid;
+    }
+
+    private void signInUser(String email, String password) {
+        if (validateForm()) {
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+    @Override
+    public void onComplete(@NonNull Task<AuthResult> task) {
+        if (task.isSuccessful()) {
+            // Sign in success, update UI
+            Log.d("TAG", "signInWithEmail:success");
+            FirebaseUser user = mAuth.getCurrentUser(); updateUI(user);
+        } else {
+            // If sign in fails, display a message to the user.
+            Log.w("TAG", "signInWithEmail:failure", task.getException());
+            Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+            updateUI(null);
+        }
+    }
+            });
+        }
+    }
+
+    private boolean isEmailValid(String email) {
+        if (!email.contains("@") || !email.contains(".") || email.length() < 5) return false;
+        return true;
     }
 }
