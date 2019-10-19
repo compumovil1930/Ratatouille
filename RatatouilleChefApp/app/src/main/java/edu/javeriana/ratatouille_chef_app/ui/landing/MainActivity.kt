871fee32eb4eb7d846d1b63ed11b7ee6fd45d10f
Juan Pablo Peñaloza Botero <juan.pa097@gmail.com>
2019-10-19 11:44:07 -0500

    finished front for login and landing page.

--- RatatouilleChefApp/app/src/main/java/edu/javeriana/ratatouille_chef_app/ui/landing/MainActivity.kt
+++ RatatouilleChefApp/app/src/main/java/edu/javeriana/ratatouille_chef_app/ui/landing/MainActivity.kt
@@ -1,13 +1,22 @@
 package edu.javeriana.ratatouille_chef_app.ui.landing
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import edu.javeriana.ratatouille_chef_app.R
+import edu.javeriana.ratatouille_chef_app.ui.login.LoginActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupButtonsToNavigate()
+    }
+
+    private fun setupButtonsToNavigate() {
+        val goToLogin = Intent(this, LoginActivity::class.java)
+        loginButton.setOnClickListener { startActivity(goToLogin) }
     }
 }
---
+++ RatatouilleChefApp/app/src/main/java/edu/javeriana/ratatouille_chef_app/ui/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package edu.javeriana.ratatouille_chef_app.ui.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import edu.javeriana.ratatouille_chef_app.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
