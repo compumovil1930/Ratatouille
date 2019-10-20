06248658913f1c1810ddbba2133959a0951a66a1
Juan Pablo Peñaloza Botero <juan.pa097@gmail.com>
2019-10-19 23:10:21 -0500

    Finished ticket.

--- RatatouilleChefApp/app/src/main/java/edu/javeriana/ratatouille_chef_app/authentication/entities/User.kt
+++ RatatouilleChefApp/app/src/main/java/edu/javeriana/ratatouille_chef_app/authentication/entities/User.kt
@@ -1,3 +1,11 @@
 package edu.javeriana.ratatouille_chef_app.authentication.entities
 
-data class User(val email: String, val password: String = "")
+data class User(
+    val email: String,
+    val password: String = "",
+    val fullName: String = "",
+    val address: LocationAddress = LocationAddress(),
+    val biography: String = "",
+    val age: Int = 0,
+    val yearsOfExperience: Int = 0
+)
--- RatatouilleChefApp/app/src/main/java/edu/javeriana/ratatouille_chef_app/authentication/repositories/AuthenticationRepository.kt
+++ RatatouilleChefApp/app/src/main/java/edu/javeriana/ratatouille_chef_app/authentication/repositories/AuthenticationRepository.kt
@@ -3,6 +3,7 @@ package edu.javeriana.ratatouille_chef_app.authentication.repositories
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import edu.javeriana.ratatouille_chef_app.authentication.entities.User
 
 interface AuthenticationRepository {
@@ -13,9 +14,14 @@ interface AuthenticationRepository {
 class FirebaseAuthenticationRepository : AuthenticationRepository {
 
     private val firebaseAuth = FirebaseAuth.getInstance()
+    private val db = FirebaseFirestore.getInstance()
+    private val usersCollection = "users"
 
     override fun createNewUser(user: User): Task<AuthResult> {
         return firebaseAuth.createUserWithEmailAndPassword(user.email, user.password)
+            .addOnSuccessListener {
+                db.document("$usersCollection/${it.user?.uid}").set(user)
+            }
     }
 
     override fun loginUserWithEmailAndPassWord(userCredentials: User): Task<AuthResult> {
