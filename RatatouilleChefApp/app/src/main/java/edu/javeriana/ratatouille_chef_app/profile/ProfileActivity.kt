592e6cc7ea54306a32b807a112956f4d667312e4
Juan Pablo Peñaloza Botero <juan.pa097@gmail.com>
2019-10-21 21:46:04 -0500

    fished profile layout.

---
+++ RatatouilleChefApp/app/src/main/java/edu/javeriana/ratatouille_chef_app/profile/ProfileActivity.kt
@@ -0,0 +1,13 @@
+package edu.javeriana.ratatouille_chef_app.profile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import edu.javeriana.ratatouille_chef_app.R
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
