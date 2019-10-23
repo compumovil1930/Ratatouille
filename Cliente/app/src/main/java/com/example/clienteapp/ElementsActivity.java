3e28fcea6e907bfe0a960452246a258cd5abd32c
David Molano <david-molano@javeriana.edu.co>
2019-10-23 00:59:12 -0500

    Created act

---
+++ Cliente/app/src/main/java/com/example/clienteapp/ElementsActivity.java
@@ -0,0 +1,14 @@
+package com.example.clienteapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ElementsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_elements);
+    }
+}
