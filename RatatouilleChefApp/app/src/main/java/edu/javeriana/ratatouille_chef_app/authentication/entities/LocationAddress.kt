c4e666b7adca8d0cea7701e28f001ebcef268932
Juan Pablo Peñaloza <juan-penaloza@javeriana.edu.co>
2019-11-17 22:57:09 -0500

    finished address picker.

--- RatatouilleChefApp/app/src/main/java/edu/javeriana/ratatouille_chef_app/authentication/entities/LocationAddress.kt
+++ RatatouilleChefApp/app/src/main/java/edu/javeriana/ratatouille_chef_app/authentication/entities/LocationAddress.kt
@@ -1,7 +1,11 @@
 package edu.javeriana.ratatouille_chef_app.authentication.entities
 
+import com.google.firebase.firestore.GeoPoint
+
+
 data class LocationAddress(
     val address: String = "",
+    val location: GeoPoint? = null,
     val latitude: Double = 0.0,
     val longitude: Double = 0.0
 )
