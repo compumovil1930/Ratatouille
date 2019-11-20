49b1551f1fca4b458295f097f7e42900505e124d
elMolda <david97molano@gmail.com>
2019-11-20 02:51:19 -0500

    Not working add element but good mockup

--- Cliente/app/src/main/java/com/example/clienteapp/ElementsActivity.java
+++ Cliente/app/src/main/java/com/example/clienteapp/ElementsActivity.java
@@ -25,21 +25,24 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ElementsActivity extends AppCompatActivity {
 
     private LinearLayout auxScrlLyt;
     private Button btnAddEl;
-
+    private Map<String, CheckBox> filler = new HashMap<>();
+    Map<String, Object> map = null;
     private FirebaseAuth mAuth;
     private FirebaseFirestore db;
 
     private View.OnClickListener btnAddLis = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            System.out.println("No");
+            addElements();
         }
     };
 
@@ -66,7 +69,7 @@ public class ElementsActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
-                                Map<String, Object> map = document.getData();
+                                map = document.getData();
                                 fillCheckboxes(map);
                             }
                         } else {
@@ -76,14 +79,42 @@ public class ElementsActivity extends AppCompatActivity {
                 });
     }
 
+    private void addElements(){
+        int totalEl = auxScrlLyt.getChildCount();
+        List<DocumentReference> utensils = new ArrayList<>();
+        for(Map.Entry<String, CheckBox> entry : filler.entrySet()){
+            if(entry.getValue().isSelected()){
+                utensils.add(db.collection("utensils").document(entry.getKey()));
+            }
+        }
+        /*for(int i = 0; i< totalEl; i++ ){
+            if(auxScrlLyt.getChildAt(i).isSelected()){
+                Toast.makeText(btnAddEl.getContext(), "TRUE", Toast.LENGTH_LONG).show();
+                CheckBox tmp = (CheckBox) auxScrlLyt.getChildAt(i);
+                String idUt = getKey(map,tmp.getText().toString());
+                utensils.add(db.collection("utensils").document(idUt));
+            }
+        }*/
+        Toast.makeText(btnAddEl.getContext(), "Utensils: "+utensils.size(), Toast.LENGTH_LONG).show();
+        db.collection("users").document(mAuth.getUid()).update("utensils",utensils);
+
+    }
+
+    private static <K, V> K getKey(Map<K, V> map, V value) {
+        for (Map.Entry<K, V> entry : map.entrySet()) {
+            if (value.equals(entry.getValue())) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+
     private void fillCheckboxes(Map<String, Object> map){
-        Map<String, CheckBox> filler = new HashMap<>();
         for(Map.Entry<String, Object> entry : map.entrySet()){
             CheckBox tmp = new CheckBox(auxScrlLyt.getContext());
             tmp.setText(entry.getValue().toString());
             filler.put(entry.getKey(), tmp);
         }
-
         for(Map.Entry<String, CheckBox> entry : filler.entrySet()){
             auxScrlLyt.addView(entry.getValue());
         }
